2c43f2982c39e5f58386bd7884df011f5b02e02f
koritacrah <juanmcoro2003@gmail.com>
2023-08-01 16:52:39 -0300

    finished landing

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -11,5 +11,16 @@ export class LandingComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
+  openWindow (opc : string) {
+    var x = ""
+    switch (opc) {
+      case "wpp":
+         x = "https://wa.me/543885148856?text=¡Hola! Estoy interesado/a en obtener más información sobre tus servicios de trekking. ¿Me puedes brindar detalles?";
+        break;
+      case "ig":
+         x = "https://www.instagram.com/canela.trekking.jujuy/";
+        break;
+    }
+    window.open(x);
+ }
 }
