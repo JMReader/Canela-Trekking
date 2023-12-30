ecb0b10ee056f8b652b1e4e84d6f32d7d5163ec3
koritacrah <juanmcoro2003@gmail.com>
2023-12-30 16:36:03 -0300

    chnaging container and side bar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContainerComponent } from './components/workerComponents/container/container.component';
 
-const routes: Routes = [];
+const routes: Routes = [ {
+  path: 'work',
+  component: ContainerComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
