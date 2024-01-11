929e303aa305e7925577a0f9d7ca6846db660fcf
koritacrah <juanmcoro2003@gmail.com>
2024-01-11 16:16:51 -0300

    update landing component
    Adding new button in landing component with facebook page

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -3,24 +3,26 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
-  styleUrls: ['./landing.component.css']
+  styleUrls: ['./landing.component.css'],
 })
 export class LandingComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-  openWindow (opc : string) {
-    var x = ""
+  ngOnInit(): void {}
+  openWindow(opc: string) {
+    var x = '';
     switch (opc) {
-      case "wpp":
-         x = "https://wa.me/543885148856?text=¡Hola! Estoy interesado/a en obtener más información sobre tus servicios de trekking. ¿Me puedes brindar detalles?";
+      case 'wpp':
+        x =
+          'https://wa.me/543885148856?text=¡Hola! Estoy interesado/a en obtener más información sobre tus servicios de trekking. ¿Me puedes brindar detalles?';
         break;
-      case "ig":
-         x = "https://www.instagram.com/canela.trekking.jujuy/";
+      case 'ig':
+        x = 'https://www.instagram.com/canela.trekking.jujuy/';
+        break;
+      case 'face':
+        x = 'https://www.facebook.com/canela.trekking.jujuy/';
         break;
     }
     window.open(x);
- }
+  }
 }
