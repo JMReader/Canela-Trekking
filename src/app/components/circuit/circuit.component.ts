d2a280fbc49374369498e963d6021190bd80ee59
koritacrah <juanmcoro2003@gmail.com>
2024-02-06 12:01:03 -0300

    adding functions
    adding transform time for changing hours to days and sanatizeDescription to public description with html

--- src/app/components/circuit/circuit.component.ts
+++ src/app/components/circuit/circuit.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Circuito } from 'src/app/models/circuito';
 import { ApiService } from 'src/app/services/api.service';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-circuit',
@@ -9,7 +10,7 @@ import { ApiService } from 'src/app/services/api.service';
 })
 export class CircuitComponent implements OnInit {
   circ !: Circuito
-  constructor(api: ApiService) {
+  constructor(api: ApiService, private sanitizer: DomSanitizer) {
     this.circ = api.getCircuits()[0]
 
    }
@@ -20,4 +21,18 @@ export class CircuitComponent implements OnInit {
     var x = "https://wa.me/543885148856?text=Hola%20buenas%20queria%20consultar%20sobre%20el%20circuito%20"+circuit+ " para la fecha de ...( completar en que fecha viaja por favor :) )";
     window.open(x);
  }
+
+ sanitizeDescription(description: string): SafeHtml {
+  return this.sanitizer.bypassSecurityTrustHtml(description);
+}
+
+//desde la api llega un numero en horas si pasa las 24 hay que transofrmarlo a dias,
+//si no lo pasa solo devuelve el numeor como string
+transformTime(time: number): string {
+  if (time > 24) {
+    return (time / 24).toFixed(0) + ' días';
+  } else {
+    return time + ' horas';
+  }
+}
 }
