65a2fd18f6f8ece2b38ede5abc32d33609504e0b
koritacrah <juanmcoro2003@gmail.com>
2024-04-27 22:55:27 -0300

    pequeños cambios
    funciona perfectamente con una estructura mucho mas limpia que antes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,24 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CircuitComponent } from './components/circuitos/circuit/circuit.component';
 import { ShowCircuitsComponent } from './components/circuitos/show-circuits/show-circuits.component';
-import { HomeComponent } from './components/circuitos/home/home.component';
 import { TilsanComponent } from './components/circuitos/tilsan/tilsan.component';
 import { LandingComponent } from './components/landing/landing.component';
 
 const routes: Routes = [ 
-  {path:'', component: LandingComponent},
+  //pagina landing lo primero que ve el usuario 
+  {path:'', component: LandingComponent}, 
+
+  // muestra todos los circuitos traidos desde la api
+  {path:'circuits', component:ShowCircuitsComponent}, 
+
+  //muestra el circuito/travesia "estrella" del emprendimiento
   {path:'circuits/tilcara-sanfrancisco', component:TilsanComponent},
-  {path:'circuits', component:ShowCircuitsComponent},
+  
+  //muestra un circuito concreto, se pasa por input
+  // el circuito pero en caso de entrar directamente al link se tra desde api
   {path:'circuits/:id', component:CircuitComponent},
+
+  //redirecciona a landing
   {path:'**', redirectTo:''}
 
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,23 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
-import { LongCardComponent } from './components/circuitos/circuits/cards/long-card/long-card.component';
+
 import { CircuitComponent } from './components/circuitos/circuit/circuit.component';
 import { ShowCircuitsComponent } from './components/circuitos/show-circuits/show-circuits.component';
-import { HomeComponent } from './components/circuitos/home/home.component';
 import { TilsanComponent } from './components/circuitos/tilsan/tilsan.component';
 import { LandingComponent } from './components/landing/landing.component';
 import { CarrouselComponent } from './shared/components/carrousel/carrousel.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
 import { HeaderComponent } from './shared/components/header/header.component';
 import { SwitchComponent } from './shared/components/switch/switch.component';
+import { LongCardComponent } from './components/circuitos/cards/long-card/long-card.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
     HeaderComponent,
     CircuitComponent,
     ShowCircuitsComponent,
