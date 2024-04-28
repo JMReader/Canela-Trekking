bd6016669cf0a43fa3d89f90d0ac8233fd9e1778
koritacrah <juanmcoro2003@gmail.com>
2024-04-27 23:02:24 -0300

    deploy 27/04
    ultima confirmacion a deployar desde aqui se trabajaran los proximos cambios de este sprint

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,27 +5,27 @@ import { ShowCircuitsComponent } from './components/circuitos/show-circuits/show
 import { TilsanComponent } from './components/circuitos/tilsan/tilsan.component';
 import { LandingComponent } from './components/landing/landing.component';
 
-const routes: Routes = [ 
-  //pagina landing lo primero que ve el usuario 
-  {path:'', component: LandingComponent}, 
-
-  // muestra todos los circuitos traidos desde la api
-  {path:'circuits', component:ShowCircuitsComponent}, 
+const routes: Routes = [
+  //pagina landing lo primero que ve el usuario
+  { path: '', component: LandingComponent },
 
   //muestra el circuito/travesia "estrella" del emprendimiento
-  {path:'circuits/tilcara-sanfrancisco', component:TilsanComponent},
-  
-  //muestra un circuito concreto, se pasa por input
-  // el circuito pero en caso de entrar directamente al link se tra desde api
-  {path:'circuits/:id', component:CircuitComponent},
+  { path: 'circuits/tilcara-sanfrancisco', component: TilsanComponent },
+
+  //esto se mostrara mas adelante cunaod se puedan ver los circuitos en la pagina
+  // // muestra todos los circuitos traidos desde la api
+  // { path: 'circuits', component: ShowCircuitsComponent },
+
+  // //muestra un circuito concreto, se pasa por input
+  // // el circuito pero en caso de entrar directamente al link se tra desde api
+  // { path: 'circuits/:id', component: CircuitComponent },
 
   //redirecciona a landing
-  {path:'**', redirectTo:''}
-
+  { path: '**', redirectTo: '' },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
