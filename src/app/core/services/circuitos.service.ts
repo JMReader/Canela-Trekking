539431788b8f12a4d288a4db7e985a83a34a4ac2
koritacrah <juanmcoro2003@gmail.com>
2024-05-04 00:40:11 -0300

    add circuit service

---
+++ src/app/core/services/circuitos.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, catchError, map, throwError } from 'rxjs';
+import { Circuit } from '../models/circuit';
+@Injectable({
+  providedIn: 'root'
+})
+
+//Service HTTP para manejar los circuitos de la aplicacion
+export class CircuitosService {
+
+  constructor(private http: HttpClient) { }
+  private apiUrl = "https://canela-trekking-server-node.onrender.com/circuit"; 
+
+  /**
+   * Fetches all circuits from the API and returns an Observable of Circuit objects.
+   */
+
+  getAllCircuits(): Observable<Circuit[]> {
+    console.log("getAllCircuits");
+    return this.http.get<Circuit[]>(this.apiUrl)
+      .pipe(
+        catchError(this.handleError), // Handle errors
+        map(data => data) // Convert response data to Circuit objects
+      );
+  }
+
+
+
+
+
+   /**
+   * Handles HTTP errors by returning an Observable error with a meaningful message.
+   * @param error The HTTP error response object.
+   */
+   private handleError(error: HttpErrorResponse): Observable<never> {
+    if (error.error instanceof ErrorEvent) {
+      // Client-side error
+      console.error('Client-side error:', error.error.message);
+    } else {
+      // Server-side error
+      console.error('Server-side error:', error.status, error.message);
+    }
+    return throwError('An error occurred while fetching circuits.');
+  }
+}
